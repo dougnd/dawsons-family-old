8931c90bf918d53a92c5ba45e1525e4fdfe61cb1
Douglas Dawson <dougnd@gmail.com>
2019-01-25 03:38:38 +0000

    Add OOA-staging

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-certbot certonly --webroot --email dougnd@gmail.com -w /var/www/letsencrypt -d dawsons.family -d www.dawsons.family -d research.dawsons.family -d recipes.dawsons.family
+certbot certonly --webroot --email dougnd@gmail.com -w /var/www/letsencrypt -d dawsons.family -d www.dawsons.family -d research.dawsons.family -d recipes.dawsons.family -d ooa-staging.dawsons.family
