77e2bc051d823ee7ea58cdba224332b437c7e8ca
Douglas Dawson <dougnd@gmail.com>
2019-07-07 17:53:51 +0000

    Add ooa prod

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-certbot certonly --webroot --email dougnd@gmail.com -w /var/www/letsencrypt -d dawsons.family -d www.dawsons.family -d research.dawsons.family -d recipes.dawsons.family -d ooa-staging.dawsons.family
+#certbot certonly --webroot --email dougnd@gmail.com -w /var/www/letsencrypt -d dawsons.family -d www.dawsons.family -d research.dawsons.family -d recipes.dawsons.family -d ooa-staging.dawsons.family 
+
+#certbot certonly --webroot --email dougnd@gmail.com -w /var/www/letsencrypt -d outofartifice.com -d analytics.outofartifice.com
+#To non-interactively renew *all* of your certificates, run
+#   "certbot renew"
+
